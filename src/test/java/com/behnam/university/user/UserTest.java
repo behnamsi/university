f71a9dae1792655ea55a0d8eaf2d468dee677db3
Behnam <behnamsaqari1981@gmail.com>
2022-09-21 21:44:09 +0430

    make user test

---
+++ src/test/java/com/behnam/university/user/UserTest.java
@@ -0,0 +1,17 @@
+package com.behnam.university.user;
+
+import com.behnam.university.model.AppUser;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Behnam Si (https://github.com/behnamsi/)
+ * @version 1.0
+ * @since 9/21/2022
+ */
+
+public class UserTest {
+    @Test
+    void addUser() {
+//        AppUser user=new AppUser()
+    }
+}
